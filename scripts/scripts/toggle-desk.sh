d5826caf2812042a3ef6da00521aaa369aa73df7
Nadeem Khedr <nadeemkhedr@gmail.com>
2025-12-28 18:43:05 +0200

    feat(leaderkey): add home shortcuts

--- scripts/scripts/toggle-desk.sh
+++ scripts/scripts/toggle-desk.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# CLI app to toggle my ikea desk
-# need to download linak-controller first
+# CLI app to toggle my ikea desk need to download linak-controller first
 # https://github.com/rhyst/linak-controller
+# To execute this file (the app running it like "leaderkey" must have bluetooth permissions in Macos)
 
 echo "===== $(date) ====="
 
